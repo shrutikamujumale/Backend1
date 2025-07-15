debfb94c67e2253e16f872d653cde08829a5e134
shrutikamujumale <mujumaleshrutika@gmail.com>
2025-07-15 21:15:37 +0530

    Add files via upload

---
+++ day4/loops.java
@@ -0,0 +1,23 @@
+package day4;
+
+public class loops {
+	public static void main(String[] args) {
+		
+//for loop
+		
+//		for(int i=0;i<=10;i++) {
+//			System.out.println(i);
+//		}
+		
+//while loop
+		
+//		int i=0;
+//		while(i<=20) {
+//			i++;
+//			System.out.println(i);
+//		}
+		
+//do while loop
+		
+	}
+}
